e4540b2a61368820aaff077264117d24328a9c92
ConstasJ <constasj@qq.com>
2023-07-16 15:38:10 +0800

    feat: 完成list功能

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,46 @@
 import e from "express";
-import { APIBody } from "./types";
+import { ListAPIBody, User } from "./types";
 import { sendEmail } from "./email";
 import morgan from "morgan";
 
 const app = e();
 
+const list: User[] = [];
+let count = 0;
+
 app.use(morgan("dev"));
 app.use(e.json());
 
-app.post('/',(req,res)=>{
-    const body = req.body as APIBody;
-    sendEmail(body.mail);
-    res.status(200);
-    res.end('success!');
+app.get('/',(req,res) => res.status(200).end('This is Sakurarealm mail system.'))
+
+app.post('/list/add', (req, res)=>{
+    const body = req.body as ListAPIBody;
+    count++;
+    list.push({
+        id: count,
+        nickname: body.nickname,
+        mail: body.mail,
+    });
+    res.status(200).end('Success!');
 })
 
-app.listen(10010, ()=>{
-    console.log('Start Listening on port 10010!');
+app.get('/list/get', (req, res) => res.status(200).json({
+    count: count,
+    list: list,
+}).end());
+
+app.post('/list/remove', (req, res) => {
+    const body = req.body as ListAPIBody;
+    try{
+        const num = list.findIndex((value) => value.nickname === body.nickname && value.mail === body.mail );
+        list.splice(num,1);
+        res.status(200).end('Success!');
+    }catch(e){
+        console.log(e);
+        res.status(500).end('Server Internal Error!');
+    }
+})
+
+app.listen(80, ()=>{
+    console.log('Start Listening on port 80!');
 });
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,11 @@
-export interface APIBody {
+export interface ListAPIBody {
     nickname: string,
     mail: string,
+}
+
+export interface User {
+    id: number,
+    nickname: string,
+    mail: string,
+    isValidated?: boolean,
 }
