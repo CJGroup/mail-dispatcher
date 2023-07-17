1b9dbe5af883cfa95f59e0bf31befff8d73f8217
ConstasJ <constasj@qq.com>
2023-07-17 12:19:39 +0800

    feat: 调整框架

--- src/email.ts
+++ src/email.ts
@@ -1,5 +1,7 @@
 import { createTransport } from "nodemailer";
-import { resolve } from "path";
+import path, { resolve } from "path";
+import * as fs from 'node:fs';
+import Hogan from 'hogan.js';
 
 const transporter = createTransport({
     host: 'smtpdm.aliyun.com',
@@ -10,15 +12,16 @@ const transporter = createTransport({
         pass: '2uLen6khqDrAz3Kh',
     }
 },{
-    from: 'oasis-auth@dm.sakura-realm.ink'
+    from: 'oasis-auth@dm.sakura-realm.ink',
+    sender: '芸樱绘境',
 });
 
 export function sendEmail(addr:string, nickname?: string){
+    const file = fs.readFileSync(resolve(__dirname,'../mail.html'),'utf-8');
+    const template = Hogan.compile(file);
     transporter.sendMail({
         to: addr,
-        html: {
-            path: resolve(__dirname,'../mail.html')
-        },
+        html: template.render({Player:nickname?nickname:'玩家'}),
         attachments: [
             {
                 filename: 'logo.png',
---
+++ src/express.ts
@@ -0,0 +1,73 @@
+import e from "express";
+import { ListAPIBody, ListSendBody, User } from "./types";
+import { sendEmail } from "./email";
+import morgan from "morgan";
+
+export function initBackend() {
+  const app = e();
+
+  const list: User[] = [];
+  let count = 0;
+
+  app.set('view engine', 'hogan.js')
+
+  app.use(morgan("dev"));
+  app.use(e.json());
+
+  app.get("/", (req, res) =>
+    res.status(200).end("This is Sakurarealm mail system.")
+  );
+
+  app.post("/list/add", (req, res) => {
+    const body = req.body as ListAPIBody;
+    count++;
+    list.push({
+      id: count,
+      nickname: body.nickname,
+      mail: body.mail,
+    });
+    res.status(200).end("Success!");
+  });
+
+  app.get("/list/get", (req, res) =>
+    res
+      .status(200)
+      .json({
+        count: count,
+        list: list,
+      })
+      .end()
+  );
+
+  app.post("/list/remove", (req, res) => {
+    const body = req.body as ListAPIBody;
+    try {
+      const num = list.findIndex(
+        (value) => value.nickname === body.nickname && value.mail === body.mail
+      );
+      list.splice(num, 1);
+      res.status(200).end("Success!");
+    } catch (e) {
+      console.log(e);
+      res.status(500).end("Server Internal Error!");
+    }
+  });
+
+  app.get("/list/send/all", (req, res) => {
+    for (const user of list) {
+      sendEmail(user.mail, user.nickname);
+    }
+  });
+
+  app.post("/list/send", (req, res) => {
+    const body = req.body as ListSendBody;
+    for (let i = 0; i < body.number; i++) {
+      const user = list[i];
+      sendEmail(user.mail, user.nickname);
+    }
+  });
+
+  app.listen(80, () => {
+    console.log("Start Listening on port 80!");
+  });
+}
