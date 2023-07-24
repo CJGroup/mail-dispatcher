2829c644439f9456af61acf161d7da2c601a7923
ConstasJ <constasj@qq.com>
2023-07-24 22:59:41 +0800

    Merge

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import morgan from "morgan";
+import cors from 'cors';
+
 import { initListAPI } from "./list";
 import { initAuthentication } from "./auth";
 import { initSend } from "./send";
@@ -10,17 +12,12 @@ export function initBackend() {
 
   app.use(morgan("dev"));
   app.use(express.json());
+  app.use(cors({
+    optionsSuccessStatus: 200,
+  }));
   app.use('/doc',express.static('./doc'));
 
-  app.use((req, res, next)=>{
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header("Access-Control-Allow-Headers", "Content-Type");
-      res.header("Access-Control-Allow-Methods", "*");
-      res.header("Content-Type", "application/json;charset=utf-8");
-      next();
-  });
-
-  app.get("/", (req, res) => res.redirect(""));
+  app.get("/", (req, res) => res.redirect("http://www.data07.cn/#/hxhj"));
   initListAPI(app);
   initSend(app);
   initAuthentication(app);
