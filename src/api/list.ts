5599a6861d29466214d5bde825456fa745ee5ed8
ConstasJ <constasj@qq.com>
2023-08-09 20:58:12 +0800

    doc: 迁移部分doc代码回到对应源码文件

--- src/api/list.ts
+++ src/api/list.ts
@@ -6,6 +6,33 @@ import { authMiddleware } from "./auth";
 
 const router = e.Router();
 
+/**
+ * @api {POST} /list/add 增加新成员
+ * @apiVersion 1.0.0
+ * @apiName Post AddMemberToList
+ * @apiGroup 列表
+ * @apiDescription 向列表中新增加成员
+ * @apiBody {String} nickname 玩家昵称
+ * @apiBody {String} mail 玩家邮件地址
+ * @apiBody {String} [serverID] 服务器ID
+ * @apiUse SuccessBase
+ * @apiSuccess {Object} book 用户预约信息
+ * @apiSuccess {Number} book.pos 当前用户在列表中的排名
+ * @apiSuccess {Number} book.total 当前列表中的用户总数
+ * @apiUse ErrorBase
+ * @apiErrorExample {json} 403 邮件已存在
+ * HTTP/1.1 403 Forbidden
+ * {
+ *    "code": 53,
+ *    "message": "This Email is already in our list!"
+ * }
+ * @apiErrorExample {json} 400 缺失参数
+ * HTTP/1.1 400 Bad Request
+ * {
+ *    "code": 40,
+ *    "message": "Missing Params!"
+ * }
+ */
 router.post("/add", async (req, res) => {
   const body = req.body as ListAPIBody;
   if ((await getList()).findIndex((val) => val.mail == body.mail) !== -1) {
@@ -40,6 +67,35 @@ router.post("/add", async (req, res) => {
   });
 });
 
+/**
+ * @api {POST} /list/add/batch 批量添加
+ * @apiVersion 1.0.0
+ * @apiName Post BatchAdd
+ * @apiGroup 列表
+ * @apiDescription 向列表中批量添加成员
+ * @apiBody {Object[]} list 批量添加的成员列表
+ * @apiBody {String} list.nickname 玩家昵称
+ * @apiBody {String} list.mail 玩家邮件地址
+ * @apiBody {String} [list.serverID] 服务器ID
+ * @apiUse SuccessBase
+ * @apiSuccess {Object} data 数据对象
+ * @apiSuccess {Object[]} data.bookList 按照顺序返回的预订信息列表
+ * @apiSuccess {Number} data.bookList.pos 当前用户在列表中的排名
+ * @apiSuccess {Number} data.bookList.total 当前列表中的用户总数
+ * @apiUse ErrorBase
+ * @apiErrorExample {json} 403 邮件已存在
+ * HTTP/1.1 403 Forbidden
+ * {
+ *    "code": 53,
+ *    "message": "This Email is already in our list!"
+ * }
+ * @apiErrorExample {json} 400 缺失参数
+ * HTTP/1.1 400 Bad Request
+ * {
+ *    "code": 40,
+ *    "message": "Missing Params!"
+ * }
+ */
 router.post("/add/batch", async (req, res) => {
   const body = req.body as ListBatchAPIBody;
   let isContinue: boolean = true;
@@ -83,6 +139,22 @@ router.post("/add/batch", async (req, res) => {
   });
 });
 
+/**
+ * @api {GET} /list/get 获取列表
+ * @apiVersion 1.0.0
+ * @apiName Get List
+ * @apiDescription 获取当前预约成员列表
+ * @apiGroup 列表
+ * @apiPermission 管理员
+ * @apiUse SuccessBase
+ * @apiSuccess {Object} data 数据对象
+ * @apiSuccess {Number} data.count 当前总用户数
+ * @apiSuccess {Object[]} data.list 预约成员列表本体
+ * @apiBody {String} data.list.nickname 玩家昵称
+ * @apiBody {String} data.list.mail 玩家邮件地址
+ * @apiBody {String} [data.list.serverID] 服务器ID
+ */
+
 router.get("/get", ...authMiddleware, async (req, res) =>
   res
     .status(200)
@@ -97,6 +169,27 @@ router.get("/get", ...authMiddleware, async (req, res) =>
     .end()
 );
 
+/**
+ * @api {GET} /list/get/self 获取预约信息
+ * @apiVersion 1.0.0
+ * @apiName Get SelfPositionInList
+ * @apiDescription 获取提供的用户在列表中的位置
+ * @apiGroup 列表
+ * @apiQuery {String} nickname 用户昵称
+ * @apiQuery {String} mail 用户邮箱
+ * @apiQUery {String} [serverID] 用户服务器ID
+ * @apiUse SuccessBase
+ * @apiSuccess {Object} data 数据对象
+ * @apiSuccess {Number} data.pos 当前用户在列表中的排名
+ * @apiSuccess {Number} data.total 当前列表中的用户总数
+ * @apiUse ErrorBase
+ * @apiErrorExample {json} 404 未找到用户
+ * HTTP/1.1 404 Not Found
+ * {
+ *    "code": 64,
+ *    "message": "No this player in the list!"
+ * }
+ */
 router.get("/get/self", async (req, res) => {
   if (!req.query.mail && !req.query.nickname) {
     res.status(500).json({
@@ -125,6 +218,19 @@ router.get("/get/self", async (req, res) => {
   }
 });
 
+/**
+ * @api {POST} /list/remove 移除成员
+ * @apiVersion 1.0.0
+ * @apiName Post RemoveListNumber
+ * @apiDescription 从列表中移除成员
+ * @apiGroup 列表
+ * @apiPermission 管理员
+ * @apiBody {String} nickname 玩家昵称
+ * @apiBody {String} mail 玩家邮件地址
+ * @apiBody {String} [serverID] 服务器ID
+ * @apiUse ErrorBase
+ * @apiUse SuccessBase
+ */
 router.post("/remove", ...authMiddleware, async (req, res) => {
   const body = req.body as ListAPIBody;
   try {
--- src/api/send.ts
+++ src/api/send.ts
@@ -7,6 +7,18 @@ import { Record } from "../db";
 
 const router = e.Router();
 
+/**
+ * @api {POST} /send 直接发送邮件
+ * @apiVersion 1.0.0
+ * @apiName Post SendEmail
+ * @apiDescription 直接向目标邮箱发送邮件
+ * @apiGroup 直接发送
+ * @apiPermission 管理员
+ * @apiBody {String} list.nickname 玩家昵称
+ * @apiBody {String} list.mail 玩家邮件地址
+ * @apiBody {String} [list.serverID] 服务器ID
+ * @apiUse SuccessBase
+ */
 router.post("/", ...authMiddleware, (req, res) => {
   const body = req.body as ListAPIBody;
   sendEmail(body.mail, body.nickname);
@@ -21,6 +33,19 @@ router.post("/", ...authMiddleware, (req, res) => {
   });
 });
 
+/**
+ * @api {POST} /send/batch 直接批量发送邮件
+ * @apiVersion 1.0.0
+ * @apiName Post BatchSendEmail
+ * @apiGroup 直接发送
+ * @apiPermission 管理员
+ * @apiDescription 批量直接向目标邮箱中发送邮件
+ * @apiBody {Object[]} list 批量发送的邮箱列表
+ * @apiBody {String} list.nickname 玩家昵称
+ * @apiBody {String} list.mail 玩家邮件地址
+ * @apiBody {String} [list.serverID] 服务器ID
+ * @apiUse SuccessBase
+ */
 router.post("/batch", ...authMiddleware, (req, res) => {
   const body = req.body as ListBatchAPIBody;
   for (const player of body.list) {
